fe96c2194f66f7b7e1b7d689411258d32da8fdc8
Morgana2000 <rupert.oxenius@googlemail.com>
2019-12-27 18:31:11 +0100

    Beauraucrat shooting different stuff

---
+++ Assets/Projectile_Naming.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_Naming : MonoBehaviour
+{
+    [SerializeField]
+    Sprite sprite0;
+    [SerializeField]
+    Sprite sprite1;
+    [SerializeField]
+    Sprite sprite2;
+    [SerializeField]
+    Sprite sprite3;
+    [SerializeField]
+    Sprite sprite4;
+    [SerializeField]
+    Sprite sprite5;
+    [SerializeField]
+    Sprite sprite6;
+    [SerializeField]
+    Sprite sprite7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprite[] sprites = { sprite0, sprite1, sprite2, sprite3, sprite4, sprite5, sprite6, sprite7 };
+         SpriteRenderer sr = this.gameObject.GetComponent<SpriteRenderer>();
+        int ran = Random.Range(0, 8);
+        sr.sprite = sprites[ran];
+        Debug.Log(ran);
+       
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
